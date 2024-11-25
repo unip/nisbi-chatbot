948b7d0bad2046af3fe2b05e86f08e180ef00bfc
Hanif <abdurrahman.alhanif@gmail.com>
2024-11-26 01:34:46 +0700

    update dummy data

--- nisbi-chatbot-plugin/src/lib/constant/dummy.ts
+++ nisbi-chatbot-plugin/src/lib/constant/dummy.ts
@@ -6,11 +6,11 @@ export const chatSessionDummy = [
     feeds: [
       {
         text: "Hallo 👋 Saya Nisbi Bot, personal assistant dari Nisbi Indonesia yang akan membantu Anda!",
-        createdAt: "1732553588",
+        createdAt: "2024-11-25T23:59",
       },
       {
         text: "Apa yang bisa saya bantu? Pilih satu dari beberapa opsi di bawah ini atau ketik pesan untuk pertanyaan lainnya.",
-        createdAt: "1732553609",
+        createdAt: "2024-11-25T23:59",
       },
     ],
   },
@@ -21,11 +21,11 @@ export const chatSessionDummy = [
     feeds: [
       {
         text: "Hallo",
-        createdAt: "1732553634",
+        createdAt: "2024-11-26T00:59",
       },
       {
         text: "Apa saja layanan yang ada di Nisbi?",
-        createdAt: "1732553642",
+        createdAt: "2024-11-26T01:03",
       },
     ],
   },
